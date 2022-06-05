8d4b36f1068f4aed6e006d25fbf5f7b51a0e4bac
hasankr <hasankr@abv.bg>
2022-06-05 18:13:21 +0300

    added 2.0

---
+++ 2. Common Elements/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Common_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr1 = Console.ReadLine().Split(' ');
+            string[] arr2 = Console.ReadLine().Split(' ');
+            foreach (var currElement in arr1)
+            {
+                for (int i = 0; i < arr2.Length; i++)
+                {
+                    string secondCurrElement = arr2[i];
+                    if (currElement == secondCurrElement)
+                    {
+                        Console.Write($"{currElement} ");
+                        break;
+                    }
+                }
+            }
+
+        }
+    }
+}
