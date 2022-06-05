3bee1f98eda4c89a13c5a5f2f83000c48cf7dcd8
hasankr <hasankr@abv.bg>
2022-06-05 17:45:07 +0300

    Added 1.0

--- 01. Train/Program.cs
+++ 01. Train/Program.cs
@@ -6,7 +6,19 @@ namespace _01._Train
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numberOfWagons = int.Parse(Console.ReadLine());
+            int[] wagons = new int[numberOfWagons];
+            int sum = 0;
+            for (int i = 0; i < wagons.Length; i++)
+            {
+                wagons[i] = int.Parse(Console.ReadLine());
+                Console.Write($"{wagons[i]} ");
+               
+                sum += wagons[i];
+               
+            }
+            Console.WriteLine();
+            Console.WriteLine(sum);
         }
     }
 }
