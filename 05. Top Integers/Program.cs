8fafea0415089fde9c151b77a4b00c8bc85bcfc6
hasankr <hasankr@abv.bg>
2022-06-05 19:41:13 +0300

    added 5.0

---
+++ 05. Top Integers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Top_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                bool currBigNum = true;
+                for (int j = i + 1; j < inputArray.Length; j++)
+                {
+                    if (inputArray[i] <= inputArray[j])
+                    {
+                        currBigNum = false;
+                    }
+                }
+                if (currBigNum)
+                {
+                    Console.Write($"{inputArray[i]} ");
+                }
+
+            }
+        }
+    }
+}
